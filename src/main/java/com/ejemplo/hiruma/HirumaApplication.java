70e3a6f4f16108c56c9ed93c980297a0ada085d2
antonioHigueron <antoniocoboscabello@gmail.com>
2021-06-06 23:27:17 +0200

    inicial

---
+++ src/main/java/com/ejemplo/hiruma/HirumaApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.hiruma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirumaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HirumaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejemplo/hiruma/HirumaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.hiruma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirumaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
